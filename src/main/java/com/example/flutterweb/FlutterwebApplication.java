d729d5dc804aeac0544ed143f05a5e3dcc2c713b
YosefAhmed <youssefahmed.cis@gmail.com>
2026-04-17 18:18:08 +0200

    test project

---
+++ src/main/java/com/example/flutterweb/FlutterwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.flutterweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlutterwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlutterwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flutterweb/FlutterwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flutterweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlutterwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
