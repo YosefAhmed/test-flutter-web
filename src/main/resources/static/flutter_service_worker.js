4ae2b295997380142470aa50e6fb7a57ca201624
YosefAhmed <youssefahmed.cis@gmail.com>
2026-04-17 19:33:32 +0200

    add test api FE

--- src/main/resources/static/flutter_service_worker.js
+++ src/main/resources/static/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "05197e6567e34906306233be0631a5cf",
+"flutter_bootstrap.js": "925c3aea75062fb169d1a4b720a5fe58",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "994b6f8955844aa2ec46aaebec6ef159",
 "/": "994b6f8955844aa2ec46aaebec6ef159",
-"main.dart.js": "4ffc48e609bb803bf00026bd3161595a",
+"main.dart.js": "88df6c4578c3b06b4990370d27d7eb36",
 "manifest.json": "57039fb4882d22159cb3739c27930397",
 "version.json": "c10777dad8a6bb08896b8adaac1c4ecc"};
 // The application shell files that are downloaded before a service worker can
